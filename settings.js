3cb4e8c73fd498e4f296b8ed7d91157102f1ac21
Bean499 <bentr99@gmail.com>
2022-03-26 16:41:08 +0000

    added ability to change icon

---
+++ settings.js
@@ -0,0 +1,8 @@
+let image = "./art/faust.png";
+let icon = "💀"
+
+let imageHTML = document.getElementById("image");
+imageHTML.innerHTML = "<img src='" + image + "'/>"
+
+let iconHTML = document.getElementById("icon");
+iconHTML.innerHTML = icon;
